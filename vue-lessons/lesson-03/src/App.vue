2626f7d1aae6f2032571199e07390a242030dba7
Yuri-Pires <yurimellopires@hotmail.com>
2022-04-11 20:08:56 -0300

    lesson 3 was finished

---
+++ vue-lessons/lesson-03/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <h1>{{ product }}</h1>
+    <img :src="image">
+    <p v-if="inventory > 10 ">In Stock</p>
+    <p v-else-if="inventory <= 10 && inventory > 0 ">Almost out of Stock</p>
+    <p v-else>Out of Stock</p>
+    <a :href="link" target="blank">link for the shop </a>
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      product: 'Socks',
+      image: 'https://a-static.mlcdn.com.br/618x463/kit-3-meias-cano-alto-estampadas-naruto-fbs/lojadivertidas/f23467fa909e11ebac5b4201ac1850e0/365a99db430732b7ad8bf659d30f503c.jpg',
+      link: 'https://shopee.com.br/Meia-Cano-Longo-Naruto-Akatsuki-Anim%C3%AA-i.349214594.5278560019',
+      inventory: 100,
+    }
+  },
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
