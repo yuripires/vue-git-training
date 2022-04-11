59de168929b9bc5908976548a75fe683dfc2e062
Yuri-Pires <yurimellopires@hotmail.com>
2022-04-11 18:53:59 -0300

    Lesson 1 foi concluida

---
+++ vue-lessons/lesson-01/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>{{ product }} </h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      product: 'Socks',
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
